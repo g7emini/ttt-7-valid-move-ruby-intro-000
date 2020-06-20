ef5d64a9613bd35096f151ab341a5d9351ed6a92
groana <contact@groanamelendez.com>
2020-06-20 22:13:18 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,8 +3,8 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 index = 0
 
-def valid_move?(board, position)
-  spot = position.to_i - 1
+def valid_move?(board, index)
+  spot = index.to_i - 1
   if !position_taken?(board, spot) && spot.between?(0,8)
 		true
 		else 

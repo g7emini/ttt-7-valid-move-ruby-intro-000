6a93d866be6dd104f9f0f6ecd8bad06c638bdf09
groana <contact@groanamelendez.com>
2020-06-20 21:54:09 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 index = 0
 
 def valid_move?(num, board)
-  num.between?(0, 8)
+  num.between?(0, 8) && 
   else
     # execute something else
   end

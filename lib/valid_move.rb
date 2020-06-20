befe89f169a5090e369da3dbbc5d0616d8e1f26e
groana <contact@groanamelendez.com>
2020-06-20 22:01:45 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 index = 0
 
 def valid_move?(board, index)
-  if !position_taken?(board, index) && (index).between?(0,8)
+  if position_taken?(board, index) && (index).between?(0,8)
     return true
   else 
     return false
